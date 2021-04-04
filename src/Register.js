3d0ad24dd32ba4ad41778753507400a49747d90f
Tanmay Mathur <36122870+Tanmay1201@users.noreply.github.com>
2021-04-04 20:33:59 +0530

    Errors message and routing

--- src/Register.js
+++ src/Register.js
@@ -1,18 +1,48 @@
 import { useState } from 'react';
 import validator from 'validator';
 import DoneIcon from '@material-ui/icons/Done';
+import { useHistory } from "react-router-dom";
+import axios from 'axios'
 import './Register.css'
+
 const Register = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [showError, setShowError] = useState(false)
+    let history = useHistory();
     var doneEmailClassName = 'IconClass'
     var doneNameClassName = 'IconClass'
     var doneConfirmPasswordClassName = 'IconClass'
     var donePasswordClassName = 'IconClass'
-    let NameSuccessStyle,EmailSuccessStyle,PasswordSuccessStyle,ConfirmPasswordSuccessStyle = {
-        
+    let NameSuccessStyle,EmailSuccessStyle,PasswordSuccessStyle,ConfirmPasswordSuccessStyle = {}
+    let isNameValidated, isEmailValidated, isPasswordValidated, isConfirmPasswordValidated;
+    const handleRegister = () => {
+        if (isNameValidated && isEmailValidated && isPasswordValidated && isConfirmPasswordValidated)
+        {
+            const data = {
+                Name: name,
+                Email: email,
+                Password: password,
+                ConfirmPassword: confirmPassword
+            }
+            axios.post('https://e-com-app-9887.herokuapp.com/register', data)
+                .then(res => {
+                if (res.data === true)
+                {
+                    history.push('/');
+                }
+            })
+        }
+        else 
+        {
+            setShowError(true)
+        }
+    }
+
+    const handleLogin = () => {
+        //redirect to login page
     }
 
     const handleNameChange = e => {
@@ -35,12 +65,23 @@ const Register = () => {
     {
         NameSuccessStyle = { border: '1px solid #80ff00' }
         doneNameClassName = 'SuccessIconClass'
+        isNameValidated = true
+    }
+    else
+    {
+        isNameValidated = false;
     }
     if (validator.isEmail(email))
     {
         EmailSuccessStyle = { border: '1px solid #80ff00' }
         doneEmailClassName = 'SuccessIconClass'
+        isEmailValidated = true
     }
+    else
+    {
+        isEmailValidated = false    
+    }
+
     if (password.length > 6)
     {
         var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
@@ -50,16 +91,33 @@ const Register = () => {
                 border: '1px solid #80ff00' 
             }
             donePasswordClassName = 'SuccessIconClass'
+            isPasswordValidated = true
+        }
+        else
+        {
+            isPasswordValidated = false    
         }
     }
+    else 
+    {
+        isPasswordValidated = false    
+    }
+
     if (confirmPassword === password && confirmPassword.length > 6)
     {
         ConfirmPasswordSuccessStyle = {
             border: '1px solid #80ff00' 
         }
         doneConfirmPasswordClassName = 'SuccessIconClass'
+        isConfirmPasswordValidated = true
     }
+    else
+    {
+        isConfirmPasswordValidated = false    
+    }
+
     return (
+        
         <div className='Register'>
             <div className='headerName'>
                 <span >REGISTER</span>
@@ -71,21 +129,26 @@ const Register = () => {
                 <input style={NameSuccessStyle}className='InputField' type='text' placeholder='Name' value={name} onChange={handleNameChange} />
                 <DoneIcon className={doneNameClassName}/>
             </div>
+            
+            {showError===true && isNameValidated===false ? <p className='ErrorMessage'>Length of name must be greater than 1</p> : '' }
             <div>
                 <input style={EmailSuccessStyle} className='InputField' type='text' placeholder='Email Id' value={ email} onChange={handleEmailChange} />
                 <DoneIcon className={doneEmailClassName}/>
             </div>
+            {showError && isEmailValidated===false ? <p className='ErrorMessage'>Incorrect Email ID</p> : ''}
             <div>
                 <input style={PasswordSuccessStyle} className='InputField' type='text' placeholder='Password' value={password} onChange={handlePasswordChange} />
                 <DoneIcon className={donePasswordClassName}/>
             </div>
+            {showError && isPasswordValidated===false ? <p className='ErrorMessage'>Password Should Contain Atleat 1 capital word, 1 unique letter and one number and  size should be greater than 6</p> : ''}
             <div>
                 <input style={ConfirmPasswordSuccessStyle} className='InputField' type='text' placeholder='Confirm Password' value={confirmPassword} onChange={handleConfirmPasswordChange} />
                 <DoneIcon className={doneConfirmPasswordClassName}/>
             </div>
+            {showError && isConfirmPasswordValidated===false ? <p className='ErrorMessage'>Passwords doesn't match</p> : '' }
             <div className='buttons'>
-                <button className='RegisterButton'>Register</button>
-                <button className='LoginButton'>Login</button>
+                <button className='RegisterButton' onClick={handleRegister}>Register</button>
+                <button className='LoginButton' onClick={handleLogin}>Login</button>
             </div>
             
         </div>
